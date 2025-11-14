c268ce8685f476be97d8ee79f6fe747bcf093e61
kks1177 <kks1177.dev@gmail.com>
2025-11-14 11:14:49 +0900

    FEAT: BE/상품 등록 기능 구현 및 axios 타임 아웃 시간 조정

--- BE/src/main/java/com/fitcaster/weatherfit/catalog/application/ItemService.java
+++ BE/src/main/java/com/fitcaster/weatherfit/catalog/application/ItemService.java
@@ -17,7 +17,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.time.LocalDate;
