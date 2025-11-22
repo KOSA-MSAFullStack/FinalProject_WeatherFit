f59fe5142a1d5a3d3a4c26c1c7c51692551844ea
lsw1096 <lsw1096@gmail.com>
2025-11-22 22:33:18 +0900

    FIX: 주문내역 쿼리 수정

--- BE/src/main/java/com/fitcaster/weatherfit/order/domain/repository/OrderRepository.java
+++ BE/src/main/java/com/fitcaster/weatherfit/order/domain/repository/OrderRepository.java
@@ -11,9 +11,7 @@ import org.springframework.data.repository.query.Param;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    @Query(value = "SELECT DISTINCT o FROM Order o " +
-            "JOIN FETCH o.orderItems oi " +
-            "JOIN FETCH oi.item i " +
+    @Query(value = "SELECT o FROM Order o " +
             "WHERE o.user.id = :userId",
             countQuery = "SELECT COUNT(o) FROM Order o WHERE o.user.id = :userId")
     Page<Order> findOrdersByUserId(@Param("userId") Long userId, Pageable pageable);
