a605a61167f4316f49bc6508e409cfa58833ce19
lsw1096 <lsw1096@gmail.com>
2025-11-22 20:10:53 +0900

    FEAT: 마이페이지 대시보드에 장바구니 수량 확인 기능 구현

--- BE/src/main/java/com/fitcaster/weatherfit/mypage/api/controller/MyPageController.java
+++ BE/src/main/java/com/fitcaster/weatherfit/mypage/api/controller/MyPageController.java
@@ -3,6 +3,7 @@ package com.fitcaster.weatherfit.mypage.api.controller;
 import com.fitcaster.weatherfit.mypage.application.ProfileService;
 import com.fitcaster.weatherfit.mypage.api.dto.request.ProfileUpdateRequest;
 import com.fitcaster.weatherfit.mypage.api.dto.response.ProfileResponse;
+import com.fitcaster.weatherfit.order.application.CartService;
 import com.fitcaster.weatherfit.user.domain.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 public class MyPageController {
 
     private final ProfileService profileService;
+    private final CartService cartService;
 
     /**
      * GET /mypage/profile (user profile 조회)
@@ -30,12 +32,22 @@ public class MyPageController {
     }
 
     /**
-     * PUT /users/profile (user profile 수정)
+     * PUT /mypage/profile (user profile 수정)
      */
     @PutMapping("/profile")
     public ResponseEntity<Void> updateUserProfile(@AuthenticationPrincipal User user, @RequestBody ProfileUpdateRequest request) {
         Long userId = user.getId();
         profileService.updateProfile(userId, request);
         return ResponseEntity.ok().build();
+    }
+
+    /**
+     * GET /mypage/cart (장바구니 상품 개수 조회)
+     */
+    @GetMapping("/cart")
+    public ResponseEntity<Integer> getCartItemCount(@AuthenticationPrincipal User user) {
+        Long userId = user.getId();
+        int count = cartService. getCartItemCount(userId);
+        return ResponseEntity.ok(count);
     }
 }
